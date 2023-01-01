dc0b0d1e3fc689e56f4bb43f3452cb1e6803c7f1
Olrando <204797682@qq.com>
2023-01-01 17:04:17 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+uni.$showMsg = function(title = '数据加载失败' , duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icom:'none',
+	})
+}
 
 Vue.config.productionTip = false
 
--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		cate
+		subpkg
 	</view>
 </template>
 
@@ -8,15 +8,12 @@
 	export default {
 		data() {
 			return {
-
-			}
-		},
-		methods: {
-			
+				
+			};
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
